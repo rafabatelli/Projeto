8773016f56a5d5a70a994c4c9ae66ec1260b631c
rafabatelli <rafaelbb4@al.insper.edu.br>
2020-04-03 16:24:16 -0300

    parte1

--- Projeto.py
+++ Projeto.py
@@ -1,1 +1,20 @@
-print('Start')
+
+import random
+fichas = 100
+aposta1=0
+aposta2=0
+aposta3=0
+aposta4=0
+
+def apostar():
+    if fichas > 0:
+        pergunta1 = input('Você deseja apostar ou sair do jogo? (apostar/sair): ')
+    if pergunta1 == 'sair'
+        return False
+    else:
+        return True       
+def sorteio():
+    dado1 = random.randint(1, 6)
+    dado2 = random.randint(1, 6)
+    soma1 = dado1 + dado2
+    return soma1
