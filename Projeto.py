4fc3e93b29e645f586af688f0e6f543cda1a0e9c
rafabatelli <rafaelbb4@al.insper.edu.br>
2020-04-03 23:52:21 -0300

    PROJETO ARQUIVO FINAL ABRA ESSE

--- Projeto.py
+++ Projeto.py
@@ -1,6 +1,6 @@
 
 import random
-fichas = 100
+fichas = 1000
 aposta1=0
 aposta2=0
 aposta3=0
@@ -8,11 +8,10 @@ aposta4=0
 soma1=0
 def apostar():
     pergunta1 = input('Você deseja apostar ou sair do jogo? (apostar/sair): ')
-    if pergunta1 == 'apostar':
+    if (pergunta1=='apostar'):
         return True
     else:
-        return False
-          
+        return False         
 def sorteio():
     dado1 = random.randint(1, 6)
     dado2 = random.randint(1, 6)
@@ -20,8 +19,8 @@ def sorteio():
     return valor
 def pass_line_bet():
     global aposta1
-    global soma1
-    global fichas   
+    global soma1 
+    global fichas  
     print('A soma do resultado dos dados é',soma1)        
     if soma1==7 or soma1==11:
         fichas = fichas + aposta1
@@ -34,7 +33,7 @@ def pass_line_bet():
     else:
         def loop_point():
             soma2=sorteio()
-            global fichas                   
+            import fichas                   
             print('O seu valor point é o {0}'.format(soma1))
             print('O novo valor sorteado é',soma2)
             print('Você possui {0} fichas agora'.format(fichas))
@@ -97,7 +96,39 @@ def twelve():
     else:
         fichas == fichas - aposta4
         print('Você possui {0} fichas agora'.format(fichas))
-        
+def apostas():
+    while True:
+        global soma1
+        soma1=sorteio()
+        pergunta2=input('Você deseja apostar em Pass Line Bet? (sim/não):')
+        if pergunta2 == 'sim':
+            aposta1 = int(input('quanto vc deseja apostar?'))
+            pass_line_bet()
+        else:
+            pass 
+        pergunta3=input('Você deseja apostar em Field? (sim/não):')
+        if pergunta3 == 'sim':
+            aposta2 = int(input('quanto vc deseja apostar?'))
+            field()
+        else:
+            pass
+        pergunta4=input('Você deseja apostar em Any Craps? (sim/não):')
+        if pergunta4 == 'sim':
+            aposta3 = int(input('quanto vc deseja apostar?'))
+            any_craps()
+        else:
+            pass
+        pergunta5=input('Você deseja apostar em Twelve? (sim/não):')
+        if pergunta5 == 'sim':
+            aposta4 = int(input('quanto vc deseja apostar?'))
+            twelve()
+        else:
+            pass 
+if (apostar()==True):
+    while(fichas>0):
+        apostas()
+else:
+    print("Obrigado por jogar")       
 
 
 
